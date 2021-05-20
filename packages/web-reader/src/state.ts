ccdda5da9d1820cef07c58147c649efe3f9f89f7
Maxime Bret <bret.maxime@gmail.com>
2021-05-20 19:23:18 +0200

    no message

---
+++ packages/web-reader/src/state.ts
@@ -0,0 +1,33 @@
+import { atom, selector } from "recoil";
+import { Reader, Manifest } from "@oboku/reader";
+
+export const bookTitleState = selector({
+  key: `bookTitleState`,
+  get: ({ get }) => {
+    return get(manifestState)?.title
+  }
+})
+
+export const bookReadyState = atom({
+  key: `bookReadyState`,
+  default: false
+})
+
+export const manifestState = atom<Manifest | undefined>({
+  key: `manifestState`,
+  default: undefined
+})
+
+export const paginationState = atom<ReturnType<Reader['getPaginationInfo']> | undefined>({
+  key: `paginationState`,
+  default: undefined
+})
+
+export const isComicState = selector({
+  key: `isComicState`,
+  get: ({ get }) => {
+    const manifest = get(manifestState)
+
+    return manifest?.renditionLayout === 'pre-paginated' || manifest?.readingOrder.every(item => item.renditionLayout === 'pre-paginated')
+  }
+})
---
+++ packages/web-reader/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'bundle': './src/index.tsx',
+    'service-worker': './src/serviceWorker/service-worker.ts',
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{
+          loader: 'ts-loader',
+          options: {
+            // This will speed up compilation enormously but disable type check
+            // make sure to run `yarn tsc` before publishing
+            transpileOnly: false,
+          }
+        }],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+    fallback: {
+      // https://github.com/lddubeau/saxes
+      stream: path.resolve(__dirname, `src/streamer/streamShim.js`)
+    }
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    // compress: true,
+    port: 9000,
+    // writeToDisk: true,
+    // open: true,
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './public',
+        }
+      ]
+    }),
+  ],
+};
