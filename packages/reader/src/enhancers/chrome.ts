f906cdc143f2df1384667fa4e8986f3da93ea2f3
Maxime Bret <bret.maxime@gmail.com>
2021-06-17 21:59:48 +0900

    fixed chrome enhancer

--- packages/reader/src/enhancers/chrome.ts
+++ packages/reader/src/enhancers/chrome.ts
@@ -1,3 +1,5 @@
+import { merge } from "rxjs";
+import { filter, takeUntil, tap } from "rxjs/operators";
 import { Enhancer } from "../createReader";
 
 /**
@@ -7,6 +9,30 @@ export const chromeEnhancer: Enhancer<{
 
 }> = (next) => (options) => {
   const reader = next(options)
+
+  /**
+   * This element is used to force a refresh of a screen in order to fix the
+   * frozen screen that happens with chrome.
+   * The bug is weird and I have yet to explain it correctly, what happens
+   * is that sometime when moving the x-axis on navigation, the screen will still display
+   * the old screen, as if it was frozen. Generally any interaction with the page will unfreeze
+   * the screen and the new iframe will be displayed. It also happens within the same iframe.
+   * So far it seems to be due to scaling and x-axis moving.
+   * To ensure the screen does not freeze we are moving a 1:1 square in & out of the screen. 
+   * That way chrome is "forced" to refresh the screen.
+   * 
+   * @todo 
+   * check https://developer.mozilla.org/en-US/docs/Web/CSS/will-change with will-change: transform
+   * to see if it does refresh all the time itself.
+   * 
+   * @todo
+   * use transform and translate rather than changing top so it only affect composite layer rather than paint
+   * @see https://www.algolia.com/blog/engineering/performant-web-animations/
+   * 
+   * @important
+   * This is disabled for now as removing will-change on container seems to fix the issue
+   */
+  // let screenForceRefreshElt: HTMLDivElement | undefined = undefined
 
   reader.manipulateContainer(container => {
     const onScroll = () => {
@@ -20,10 +46,46 @@ export const chromeEnhancer: Enhancer<{
      */
     container.addEventListener('scroll', onScroll)
 
+    // screenForceRefreshElt = container.ownerDocument.createElement('div')
+    // screenForceRefreshElt.style.cssText = `
+    //   position: absolute;
+    //   background-color: black;
+    //   left: 0;
+    //   top: -1px;
+    //   width: 1px;
+    //   height: 1px;
+    // `
+
+    // container.appendChild(screenForceRefreshElt)
+
     return () => {
       container.removeEventListener('scroll', onScroll)
     }
   })
+
+  reader.registerHook(`readingItem.onLoad`, ({ frame }) => {
+    /**
+     * Disable touch to search on first text touch / click. This does not prevent
+     * it when selecting text. It needs to be turned off by the user.
+     * @see https://developers.google.com/web/updates/2015/10/tap-to-search
+     */
+    frame.contentDocument?.body.setAttribute(`tabindex`, `-1`)
+  })
+
+  const forceScreenRefresh$ = reader.$
+    .pipe(
+      filter(event => event.type === `onNavigationChange`),
+      tap(() => {
+        // screenForceRefreshElt?.style.setProperty(`top`, `0px`)
+        // requestAnimationFrame(() => {
+        //   screenForceRefreshElt?.style.setProperty(`top`, `-1px`)
+        // })
+      })
+    )
+
+  merge(forceScreenRefresh$)
+    .pipe(takeUntil(reader.destroy$))
+    .subscribe()
 
   return reader
 }
