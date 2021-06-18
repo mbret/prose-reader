de1e5ecfbd40264003c981758c9293720ba034e2
Maxime Bret <bret.maxime@gmail.com>
2021-06-18 18:18:59 +0900

    no message

--- packages/doc/docusaurus.config.js
+++ packages/doc/docusaurus.config.js
@@ -59,19 +59,6 @@ module.exports = {
             },
           ],
         },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
