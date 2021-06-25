37dd96234857ee304b9e2d273d22713aabb17ad2
Maxime Bret <bret.maxime@gmail.com>
2021-06-25 21:23:00 +0900

    remove rn from package

--- packages/ReactNativeReader/web/src/messages.ts
+++
@@ -1,8 +0,0 @@
-import { Subject } from "rxjs"
-import { MessageDown, MessageUp } from "./types/shared"
-
-export const messagesDown$ = new Subject<MessageDown>()
-
-export const postMessage = (message: MessageUp) => {
-  window.ReactNativeWebView?.postMessage(JSON.stringify(message))
-}
--- packages/ReactNativeReader/web/src/types/shared.ts
+++
@@ -1,3 +0,0 @@
-export type MessageDown = { event: 'goTo', data: number }
-
-export type MessageUp = { event: 'menuTap' }
