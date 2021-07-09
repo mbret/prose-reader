0f97e5a68f35b356dd67c4fb171f3a21d417ef4f
Maxime Bret <bret.maxime@gmail.com>
2021-07-09 14:22:36 +0900

    added audio book

--- packages/web-reader/src/classic/Home.tsx
+++ packages/web-reader/src/classic/Home.tsx
@@ -9,56 +9,67 @@ export const Home = () => {
   return (
     <div style={{
       height: `100%`,
-      width: `100%`,
-      margin: `auto`,
-      maxWidth: 320,
-      display: `flex`,
-      flexDirection: `column`,
-      alignItems: `center`
+      overflow: `scroll`
     }}>
-      <p style={{ alignSelf: 'flex-start' }}>
-        <b>LTR</b> = left to right, <b>RTL</b> = right to left
-        <br /><b>RFL</b> = fully reflowable
-        <br /><b>RFL(P)</b> = partially reflowable
-        <br /><b>FXL</b> = fully pre-paginated (fixed layout)
-        <br /><b>FXL(P)</b> = partially pre-paginated (fixed layout)
-        <br /><b>TXT</b> = .txt file (RFL)
-      </p>
-      <p style={{ width: `100%`, display: `flex` }}>
-        <input type="text" placeholder="Paste your link to epub,cbz,txt,..." style={{ flex: 1, marginRight: 10, padding: 5 }} onChange={e => setCustomUrl(e.target.value)} />
-        <button onClick={() => {
-          customUrl.length > 0 && history.push(`/classic/reader/${btoa(customUrl)}`)
-        }}>open</button>
-      </p>
-      <Row
-        style={{ borderTop: `1px solid black`, }}
-        url={`${window.location.origin}/epubs/accessible_epub_3.epub`}
-        name="accessible_epub_3.epub"
-        details="EN - LTR - RFL" />
-      <Row
-        url={`${window.location.origin}/epubs/sous-le-vent.epub`}
-        name="sous-le-vent.epub"
-        details="EN - LTR - FXL" />
-      <Row
-        url={`${window.location.origin}/epubs/moby-dick_txt.txt`}
-        name="moby-dick_txt.txt"
-        details="EN - LTR - TXT" />
-      <Row
-        url={`${window.location.origin}/epubs/mymedia_lite.epub`}
-        name="mymedia_lite.epub"
-        details="JP - RTL - RFL" />
-      <Row
-        url={`${window.location.origin}/epubs/haruko-html-jpeg.epub`}
-        name="haruko-html-jpeg.epub"
-        details="JP - RTL - FXL(P)" />
-      <Row
-        url={`${window.location.origin}/epubs/regime-anticancer-arabic.epub`}
-        name="regime-anticancer-arabic.epub"
-        details="AR - RTL - RFL" />
-      <Row
-        url={`${window.location.origin}/epubs/sample.cbz`}
-        name="sample.cbz"
-        details="EN - LTR - FXL" />
+      <div style={{
+        width: `100%`,
+        margin: `auto`,
+        maxWidth: 320,
+        display: `flex`,
+        flexDirection: `column`,
+        alignItems: `center`,
+        marginTop: 20,
+        marginBottom: 20,
+      }}>
+        <p style={{ alignSelf: 'flex-start' }}>
+          <b>LTR</b> = left to right, <b>RTL</b> = right to left
+          <br /><b>RFL</b> = fully reflowable
+          <br /><b>RFL(P)</b> = partially reflowable
+          <br /><b>FXL</b> = fully pre-paginated (fixed layout)
+          <br /><b>FXL(P)</b> = partially pre-paginated (fixed layout)
+          <br /><b>TXT</b> = .txt file (RFL)
+          <br /><b>MEDIA</b> = contains media (audio, video)
+        </p>
+        <p style={{ width: `100%`, display: `flex` }}>
+          <input type="text" placeholder="Paste your link to epub,cbz,txt,..." style={{ flex: 1, marginRight: 10, padding: 5 }} onChange={e => setCustomUrl(e.target.value)} />
+          <button onClick={() => {
+            customUrl.length > 0 && history.push(`/classic/reader/${btoa(customUrl)}`)
+          }}>open</button>
+        </p>
+        <Row
+          style={{ borderTop: `1px solid black`, }}
+          url={`${window.location.origin}/epubs/accessible_epub_3.epub`}
+          name="accessible_epub_3.epub"
+          details="EN - LTR - RFL" />
+        <Row
+          url={`${window.location.origin}/epubs/sous-le-vent.epub`}
+          name="sous-le-vent.epub"
+          details="EN - LTR - FXL" />
+        <Row
+          url={`${window.location.origin}/epubs/moby-dick_txt.txt`}
+          name="moby-dick_txt.txt"
+          details="EN - LTR - TXT" />
+        <Row
+          url={`${window.location.origin}/epubs/mymedia_lite.epub`}
+          name="mymedia_lite.epub"
+          details="JP - RTL - RFL" />
+        <Row
+          url={`${window.location.origin}/epubs/haruko-html-jpeg.epub`}
+          name="haruko-html-jpeg.epub"
+          details="JP - RTL - FXL(P)" />
+        <Row
+          url={`${window.location.origin}/epubs/regime-anticancer-arabic.epub`}
+          name="regime-anticancer-arabic.epub"
+          details="AR - RTL - RFL" />
+        <Row
+          url={`${window.location.origin}/epubs/sample.cbz`}
+          name="sample.cbz"
+          details="EN - LTR - FXL" />
+        <Row
+          url={`${window.location.origin}/epubs/cc-shared-culture.epub`}
+          name="cc-shared-culture.epub"
+          details="EN - LTR - MEDIA" />
+      </div>
     </div>
   )
 }
