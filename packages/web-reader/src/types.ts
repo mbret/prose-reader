f433a27f980cf25452c102bcf052913f2eef38a9
Maxime Bret <bret.maxime@gmail.com>
2021-07-09 19:35:12 +0900

    no message

--- packages/web-reader/src/types.ts
+++ packages/web-reader/src/types.ts
@@ -1,7 +1,7 @@
-import type { createBookmarksEnhancer } from "@oboku/reader-enhancer-bookmarks";
-import type { searchEnhancer } from "@oboku/reader-enhancer-search";
 import { ReaderWithEnhancer } from "@oboku/reader";
-import { createHighlightsEnhancer } from "../../enhancer-highlights/src";
+import { createBookmarksEnhancer } from "@oboku/reader-enhancer-bookmarks";
+import { searchEnhancer } from "@oboku/reader-enhancer-search";
+import { createHighlightsEnhancer } from "@oboku/reader-enhancer-highlights";
 import { ComposeEnhancer } from "@oboku/reader/dist/utils/composeEnhancer";
 
 export type ReaderInstance = ReaderWithEnhancer<ComposeEnhancer<typeof searchEnhancer, ReturnType<typeof createBookmarksEnhancer>, ReturnType<typeof createHighlightsEnhancer>>>
