e834afeaff511b482cab4bede8de500b9741eff6
Maxime Bret <bret.maxime@gmail.com>
2021-10-08 12:42:51 +0900

    no message

--- packages/streamer/src/index.ts
+++ packages/streamer/src/index.ts
@@ -1,3 +1,4 @@
 export { getResourceFromArchive } from './generators/resources'
 export { getManifestFromArchive } from './generators/manifest'
 export { Archive, createArchiveFromText, createArchiveFromUrls, createArchiveFromJszip, createArchiveFromArrayBufferList } from './archives'
+export { Manifest } from '@oboku/shared'
