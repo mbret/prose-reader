f42d5aaa6e71eeed22578f50f93606d5b4c856db
Maxime Bret <bret.maxime@gmail.com>
2021-11-07 16:59:38 +0900

    fixed react wrapper layout detection

--- packages/react/src/Reader.tsx
+++ packages/react/src/Reader.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useEffect, useMemo, useRef, useState } from "react"
 import { createReader, Enhancer, Manifest, Reader } from '@oboku/reader'
 
 type Options = Parameters<typeof createReader>[0]
 type LoadOptions = Parameters<ReturnType<typeof createReader>['load']>[1]
 type Pagination = ReturnType<ReturnType<typeof createReader>['pagination']['getInfo']>
-type EnhancerOptions<E extends Enhancer | void> = E extends Enhancer ? Parameters<ReturnType<E>>[0] & Options : Options 
+type EnhancerOptions<E extends Enhancer | void> = E extends Enhancer ? Parameters<ReturnType<E>>[0] & Options : Options
 
 type Props<UserEnhancer extends Enhancer | void> = {
   manifest?: Manifest,
@@ -14,19 +14,22 @@ type Props<UserEnhancer extends Enhancer | void> = {
   onReader?: (reader: Reader<UserEnhancer>) => void,
   onReady?: () => void,
   onPaginationChange?: (pagination: Pagination) => void,
+  layout?: { height: number, width: number } | `cover`
 }
 
-export function Reader<UserEnhancer extends Enhancer | void>({ manifest, onReady, onReader, loadOptions, options, onPaginationChange, enhancer }: Props<UserEnhancer>) {
+export function Reader<UserEnhancer extends Enhancer | void>({ layout = `cover`, manifest, onReady, onReader, loadOptions, options, onPaginationChange, enhancer }: Props<UserEnhancer>) {
   const [reader, setReader] = useState<Reader<UserEnhancer> | undefined>(undefined)
+  const { width, height } = layout === `cover` ? { width: `100%`, height: `100%` } : layout
+  const ref = useRef<HTMLElement>()
 
-  const onRef = useCallback(ref => {
-    if (ref && !reader) {
-      const readerOptions = { containerElement: ref, ...options }
+  useEffect(() => {
+    if (ref.current && !reader) {
+      const readerOptions = { containerElement: ref.current, ...options }
       const reader: any = enhancer ? createReader(readerOptions, enhancer) : createReader(readerOptions)
       setReader(reader)
       onReader && onReader(reader)
     }
-  }, [setReader, onReader, reader, options])
+  }, [ref.current, setReader, onReader, reader, options])
 
   useEffect(() => {
     const readerSubscription$ = reader?.$.ready$.subscribe(() => {
@@ -54,21 +57,39 @@ export function Reader<UserEnhancer extends Enhancer | void>({ manifest, onReady
     }
   }, [manifest, reader, loadOptions])
 
+  useReaderReLayout(reader, ref.current, height, width)
+
   useEffect(() => {
     return () => reader?.destroy()
   }, [reader])
 
+  const style = useMemo(() => ({
+    width,
+    height
+  }), [height, width])
+
   return (
     <div
-      style={styles.reader}
-      ref={onRef}
+      style={style}
+      ref={ref as any}
     />
   )
 }
 
-const styles = {
-  reader: {
-    width: `100%`,
-    height: `100%`
-  }
+const useReaderReLayout = (reader: Reader | undefined, container: HTMLElement | undefined, height: number | string, width: number | string) => {
+  useEffect(() => {
+    if (typeof width === `number` && typeof height === `number`) {
+      reader?.layout()
+    } else if (container) {
+      const observer = new ResizeObserver(() => {
+        reader?.layout()
+      })
+
+      observer.observe(container)
+
+      return () => {
+        observer.disconnect()
+      }
+    }
+  }, [height, width, container])
 }
