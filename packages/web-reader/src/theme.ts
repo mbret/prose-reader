e7519900f32e3e79690d0d4d037546e92d15ea62
Maxime Bret <bret.maxime@gmail.com>
2021-11-10 17:00:24 +0900

    use dark mode by default

--- packages/web-reader/src/theme.ts
+++ packages/web-reader/src/theme.ts
@@ -1,5 +1,12 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme, ThemeConfig } from "@chakra-ui/react"
+
+const config: ThemeConfig = {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+}
 
 export const theme = extendTheme({
-
+    config
 })
+
+console.log(theme)
