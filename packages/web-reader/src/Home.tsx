8cc02bc1c25d33c2f85063d247660edcf1507e32
Maxime Bret <bret.maxime@gmail.com>
2021-11-10 18:54:55 +0900

    improved home design

--- packages/web-reader/src/Home.tsx
+++ packages/web-reader/src/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button as ChakraButton, Text } from "@chakra-ui/react"
+import { Button as ChakraButton, Text, Box } from "@chakra-ui/react"
 import { ExternalLinkIcon } from "@chakra-ui/icons"
 
 export const Home = () => {
@@ -10,7 +10,6 @@ export const Home = () => {
       height: `100%`,
     }}>
       <div style={{
-        // height: `100%`,
         width: `100%`,
         margin: `auto`,
         paddingTop: 10,
@@ -21,45 +20,29 @@ export const Home = () => {
         alignItems: `center`,
         justifyContent: 'center'
       }}>
-        <div style={{
-          border: `2px solid red`,
-          // backgroundColor: `#f7e5e5`,
-          padding: 10,
-          borderRadius: 10,
-          marginBottom: 10,
-          width: `100%`,
-        }}>
+        <Box padding={2} textAlign="justify" maxWidth={[`auto`]} marginX={[4]} mb={4}>
           <Text>
-            This reader is a demo made with React and use <b>oboku-reader</b> to render its content. 
+            This reader is a demo made with React and use <b>oboku-reader</b> to render its content.
             It provides a simple/generic user interface to showcase what <b>oboku-reader</b> can do.
             It is optimized to be viewed on mobile.
           </Text>
-        </div>
-        <div style={{
-          border: `2px solid #0066ff`,
-          // backgroundColor: `#f7e5e5`,
-          padding: 10,
-          borderRadius: 10,
-          marginBottom: 10,
-          width: `100%`,
-        }}>
-          <Link to={`/classic`} style={{ marginBottom: 20 }}>
-            <ChakraButton rightIcon={<ExternalLinkIcon />}>Visit the classic reader</ChakraButton>
-          </Link>
+        </Box>
+        <Box borderWidth={1} borderRadius={10} padding={4} maxWidth={[`auto`]} marginX={[4]} mb={4}>
+          <Box mb={2}>
+            <Link to={`/classic`} style={{ marginBottom: 20 }}>
+              <ChakraButton rightIcon={<ExternalLinkIcon />}>Visit the classic reader</ChakraButton>
+            </Link>
+          </Box>
           <Text>Showcase suitable for any type of content</Text>
-        </div>
-        <div style={{
-          border: `2px solid #0066ff`,
-          // backgroundColor: `#f7e5e5`,
-          padding: 10,
-          borderRadius: 10,
-          width: `100%`,
-        }}>
-          <Link to={`/comics`} >
-            <ChakraButton rightIcon={<ExternalLinkIcon />}>Visit the comic reader</ChakraButton>
-          </Link>
+        </Box>
+        <Box borderWidth={1} borderRadius={10} padding={4} maxWidth={[`auto`]} marginX={[4]} mb={4}>
+          <Box mb={2}>
+            <Link to={`/comics`} >
+              <ChakraButton rightIcon={<ExternalLinkIcon />}>Visit the comic reader</ChakraButton>
+            </Link>
+          </Box>
           <Text>Showcase suitable for manga services</Text>
-        </div>
+        </Box>
       </div>
     </div>
   )
