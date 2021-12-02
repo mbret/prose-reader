63406a2e1ee0b11cfe3a446577fb33cce406ed1d
Maxime Bret <bret.maxime@gmail.com>
2021-12-02 17:23:12 +0100

    improve build

--- packages/streamer/webpack.config.js
+++ packages/streamer/webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
           loader: `ts-loader`,
           options: {
             compilerOptions: {
+              noEmit: false,
               declaration: true
             }
           }
