0b53ea9ba992b202b99f76613fa19255a3f5651e
mbret <bret.maxime@gmail.com>
2022-04-03 21:34:05 +0200

    remove android code

--- ReactNativeReader/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'react-native-static-server' {
-  export default class StaticServer {
-    constructor(port: number, path?: string, options?: { localOnly: boolean })
-    start: () => Promise<string>
-    stop: () => void
-    isRunning: () => Promise<boolean>
-  }
-}
