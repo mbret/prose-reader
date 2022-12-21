57f06929ec054a2d06459b6d55e0ac880e6b092f
Maxime Bret <bret.maxime@gmail.com>
2022-12-21 13:56:41 +0100

    fix: misc

--- packages/enhancer-hammer-gesture/src/index.ts
+++ packages/enhancer-hammer-gesture/src/index.ts
@@ -1,52 +1,58 @@
 import { Enhancer } from "@prose-reader/core"
-import HammerStatic from "hammerjs"
-
-class HammerManager extends HammerStatic.Manager { }
+import "hammerjs"
 
 /**
  */
-export const hammerGestureEnhancer: Enhancer<{
-  hammerManager?: HammerManager
-}, {}> = (next) => ({ hammerManager, ...rest }) => {
-  const reader = next(rest)
+export const hammerGestureEnhancer: Enhancer<
+  {
+    // eslint-disable-next-line no-undef
+    hammerManager?: HammerManager
+  },
+  {}
+> =
+  (next) =>
+    ({ hammerManager, ...rest }) => {
+      const reader = next(rest)
 
-  const onPinchStart = () => {
-    if (!reader?.zoom.isZooming()) {
-      reader?.zoom.enter()
-    }
-  }
+      const onPinchStart = () => {
+        if (!reader?.zoom.isZooming()) {
+          reader?.zoom.enter()
+        }
+      }
 
-  const onPinchEnd = (ev: HammerStatic[`Input`]) => {
-    if (reader?.zoom.isZooming()) {
-      reader?.zoom.setCurrentScaleAsBase()
-      if (
-        reader?.zoom.isUsingScrollableZoom() &&
-        reader?.zoom.getScaleValue() <= 1
-      ) {
-        reader?.zoom.exit()
+      // eslint-disable-next-line no-undef
+      const onPinchEnd = (ev: HammerInput) => {
+        if (reader?.zoom.isZooming()) {
+          reader?.zoom.setCurrentScaleAsBase()
+          if (
+            reader?.zoom.isUsingScrollableZoom() &&
+          reader?.zoom.getScaleValue() <= 1
+          ) {
+            reader?.zoom.exit()
+          }
+        }
+      }
+
+      // eslint-disable-next-line no-undef
+      const onPinch = (ev: HammerInput) => {
+        if (reader?.zoom.isZooming()) {
+          reader?.zoom.scale(ev.scale)
+        }
+      }
+
+      hammerManager?.on(`pinch`, onPinch)
+      hammerManager?.on(`pinchstart`, onPinchStart)
+      hammerManager?.on(`pinchend`, onPinchEnd)
+
+      const destroy = () => {
+        hammerManager?.off(`pinchstart`, onPinchStart)
+        hammerManager?.off(`pinchend`, onPinchEnd)
+        hammerManager?.off(`pinch`, onPinch)
+        reader.destroy()
+      }
+
+      return {
+        ...reader,
+        destroy
       }
     }
-  }
-
-  const onPinch = (ev: HammerStatic[`Input`]) => {
-    if (reader?.zoom.isZooming()) {
-      reader?.zoom.scale(ev.scale)
-    }
-  }
-
-  hammerManager?.on(`pinch`, onPinch)
-  hammerManager?.on(`pinchstart`, onPinchStart)
-  hammerManager?.on(`pinchend`, onPinchEnd)
-
-  const destroy = () => {
-    hammerManager?.off(`pinchstart`, onPinchStart)
-    hammerManager?.off(`pinchend`, onPinchEnd)
-    hammerManager?.off(`pinch`, onPinch)
-    reader.destroy()
-  }
-
-  return {
-    ...reader,
-    destroy
-  }
-}
