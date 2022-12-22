ea1486e6ce4cdfa8b57b94bd1981a3dddfef18b6
Maxime Bret <bret.maxime@gmail.com>
2022-12-22 10:28:29 +0100

    fix: foo

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,5 @@
 export type { Manifest, TocItem } from './Manifest'
 export { detectContentType, parseContentType } from './contentType'
 export { getUrlExtension } from './url'
+
+console.log("asd")
