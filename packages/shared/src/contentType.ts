3d59e1bb2d159f173f4f1beeda5bb811dcc7df7b
Maxime Bret <bret.maxime@gmail.com>
2022-12-24 07:19:40 +0100

    feat: added xml helper in shared

--- packages/shared/src/contentType.ts
+++ packages/shared/src/contentType.ts
@@ -1,23 +1,42 @@
-import { getUrlExtension } from "./url"
+import { getUrlExtension } from "./url";
 
-export const detectContentType = (name: string) => {
-  const extension = getUrlExtension(name)
+export const detectMimeTypeFromName = (name: string) => {
+  const extension = getUrlExtension(name);
 
   switch (extension) {
-    case `png`: return `image/png`
-    case `jpg`: return `image/jpg`
-    case `jpeg`: return `image/jpeg`
-    case `txt`: return `text/plain`
-    case `webp`: return `image/webp`
+    case `png`:
+      return `image/png`;
+    case `jpg`:
+      return `image/jpg`;
+    case `jpeg`:
+      return `image/jpeg`;
+    case `txt`:
+      return `text/plain`;
+    case `webp`:
+      return `image/webp`;
+    case `xhtml`:
+      return `application/xhtml+xml`;
   }
 
-  return undefined
-}
+  return undefined;
+};
+
+export const isXmlBasedMimeType = ({
+  mimeType,
+  uri,
+}: {
+  uri?: string;
+  mimeType?: string;
+}) => {
+  const _mimeType = mimeType ?? detectMimeTypeFromName(uri ?? "");
+
+  return _mimeType?.startsWith(`application/xhtml+xml`);
+};
 
 export const parseContentType = (str: string) => {
-  if (!str.length) return undefined
+  if (!str.length) return undefined;
 
-  const cut = str.indexOf(`;`)
+  const cut = str.indexOf(`;`);
 
-  return cut ? str.substring(0, str.indexOf(`;`)) : str
-}
+  return cut ? str.substring(0, str.indexOf(`;`)) : str;
+};
