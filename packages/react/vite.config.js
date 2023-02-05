7f4c59965fdefa1839637955470f6f9b6e8ca08c
Maxime Bret <bret.maxime@gmail.com>
2023-02-05 09:45:00 +0100

    feat: migrated react build to vite

---
+++ packages/react/vite.config.js
@@ -0,0 +1,29 @@
+// vite.config.js
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+import { externals } from "rollup-plugin-node-externals"
+
+export default defineConfig(({ mode }) => ({
+  build: {
+    lib: {
+      entry: resolve(__dirname, `src/index.ts`),
+      name: `prose-react`,
+      fileName: `prose-react`,
+    },
+    minify: false,
+    sourcemap: true,
+    emptyOutDir: mode !== `development`,
+  },
+  plugins: [
+    {
+      enforce: "pre",
+      ...externals({
+        peerDeps: true,
+        deps: true,
+        devDeps: true,
+      }),
+    },
+    dts(),
+  ],
+}))
