b9fa7a462a99eb816851184014809284bc9b897f
Maxime Bret <bret.maxime@gmail.com>
2023-02-11 23:40:39 +0100

    fix: fixed service worker dev registration

--- packages/prose-reader-demo/src/main.tsx
+++ packages/prose-reader-demo/src/main.tsx
@@ -13,7 +13,9 @@ const queryClient = new QueryClient()
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js", {})
+      .register(import.meta.env.MODE === "production" ? "/service-worker.js" : "/dev-sw.js?dev-sw", {
+        type: import.meta.env.MODE === "production" ? "classic" : "module"
+      })
       .then((registration) => {
         console.log("SW registered: ", registration)
         if (!container) return
