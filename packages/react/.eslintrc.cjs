400d429b068b3157cab7d04dafbfeb25f33ed466
maxime <bret.maxime@gmail.com>
2024-05-27 13:01:08 +0200

    feat: added back reader package

---
+++ packages/react/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: [`plugin:jest/recommended`, `prettier`, `plugin:react/recommended`],
+  parser: `@typescript-eslint/parser`,
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: `module`,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [`jest`, `@typescript-eslint`, `react`],
+}
---
+++ packages/react/vite.config.ts
@@ -0,0 +1,40 @@
+// vite.config.js
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+import externals from "rollup-plugin-node-externals"
+import { name } from "./package.json"
+import react from "@vitejs/plugin-react"
+
+const libName = name.replace(`@`, ``).replace(`/`, `-`)
+
+export default defineConfig(({ mode }) => ({
+  build: {
+    minify: false,
+    lib: {
+      entry: resolve(__dirname, `src/index.ts`),
+      name: libName,
+      fileName: "index",
+    },
+    emptyOutDir: mode !== `development`,
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    {
+      enforce: `pre`,
+      ...externals({
+        peerDeps: true,
+        deps: true,
+        devDeps: true,
+      }),
+    },
+    dts({
+      entryRoot: "src",
+      compilerOptions: {
+        declaration: false,
+        skipLibCheck: true,
+      },
+    }),
+  ],
+}))
