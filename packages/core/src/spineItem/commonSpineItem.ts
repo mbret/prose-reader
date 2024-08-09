5ad88bb451b81cf9693fd93c6e3218147b57c4c5
Maxime Bret <bret.maxime@gmail.com>
2024-08-09 15:21:26 +0200

    feat: rename

--- packages/core/src/spineItem/commonSpineItem.ts
+++ packages/core/src/spineItem/commonSpineItem.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Context } from "../context/Context"
-import { createFrameItem } from "./frame/frameItem"
+import { createFrameItem } from "./frame/frameItems"
 import { Manifest } from ".."
 import { Subject } from "rxjs"
 import { createFingerTracker, createSelectionTracker } from "./trackers"
