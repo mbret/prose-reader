8da6fd114257fed58ca7927ee2d70c2b8d7482e7
Maxime Bret <bret.maxime@gmail.com>
2024-08-11 10:26:01 +0200

    feat: added new navigation state

--- packages/core/src/enhancers/navigation/state.ts
+++ packages/core/src/enhancers/navigation/state.ts
@@ -7,32 +7,53 @@ export type State = ReturnType<typeof observeState>
 export const observeState = (reader: Reader) => {
   return reader.pagination.pagination$.pipe(
     withLatestFrom(reader.context.manifest$, reader.settings.settings$),
-    map(([paginationInfo, manifest, { computedPageTurnDirection }]) => {
-      const numberOfSpineItems = manifest?.spineItems.length ?? 0
-      const isAtAbsoluteBeginning = paginationInfo.beginSpineItemIndex === 0
-      const isAtAbsoluteEnd =
-        paginationInfo.endSpineItemIndex === Math.max(numberOfSpineItems - 1, 0)
+    map(
+      ([
+        paginationInfo,
+        { spineItems, readingDirection },
+        { computedPageTurnDirection },
+      ]) => {
+        const numberOfSpineItems = spineItems.length ?? 0
+        const isAtAbsoluteBeginning = paginationInfo.beginSpineItemIndex === 0
+        const isAtAbsoluteEnd =
+          paginationInfo.endSpineItemIndex ===
+          Math.max(numberOfSpineItems - 1, 0)
 
-      const isAtEndSpineItem =
-        paginationInfo.endSpineItemIndex === Math.max(numberOfSpineItems - 1, 0)
+        const isAtEndSpineItem =
+          paginationInfo.endSpineItemIndex ===
+          Math.max(numberOfSpineItems - 1, 0)
 
-      const isAtBeginSpineItem = paginationInfo.beginSpineItemIndex === 0
+        const isAtBeginSpineItem = paginationInfo.beginSpineItemIndex === 0
 
-      return {
-        canGoTopSpineItem:
-          computedPageTurnDirection === "vertical" && !isAtAbsoluteBeginning,
-        canGoBottomSpineItem:
-          computedPageTurnDirection === "vertical" && !isAtAbsoluteEnd,
-        canGoLeftSpineItem:
-          computedPageTurnDirection !== "vertical" &&
-          ((manifest?.readingDirection === "ltr" && !isAtAbsoluteBeginning) ||
-            (manifest?.readingDirection === "rtl" && !isAtEndSpineItem)),
-        canGoRightSpineItem:
-          computedPageTurnDirection !== "vertical" &&
-          ((manifest?.readingDirection === "ltr" && !isAtAbsoluteEnd) ||
-            (manifest?.readingDirection === "rtl" && !isAtBeginSpineItem)),
-      }
-    }),
+        const isAtBeginFirstPage =
+          paginationInfo.beginPageIndexInSpineItem === 0
+
+        const isAtEndLastPage =
+          paginationInfo.endPageIndexInSpineItem ===
+          paginationInfo.endNumberOfPagesInSpineItem - 1
+
+        return {
+          canTurnLeft:
+            computedPageTurnDirection === "vertical"
+              ? false
+              : !isAtBeginFirstPage,
+          canTurnRight:
+            computedPageTurnDirection === "vertical" ? false : !isAtEndLastPage,
+          canGoTopSpineItem:
+            computedPageTurnDirection === "vertical" && !isAtAbsoluteBeginning,
+          canGoBottomSpineItem:
+            computedPageTurnDirection === "vertical" && !isAtAbsoluteEnd,
+          canGoLeftSpineItem:
+            computedPageTurnDirection !== "vertical" &&
+            ((readingDirection === "ltr" && !isAtAbsoluteBeginning) ||
+              (readingDirection === "rtl" && !isAtEndSpineItem)),
+          canGoRightSpineItem:
+            computedPageTurnDirection !== "vertical" &&
+            ((readingDirection === "ltr" && !isAtAbsoluteEnd) ||
+              (readingDirection === "rtl" && !isAtBeginSpineItem)),
+        }
+      },
+    ),
     distinctUntilChanged(isShallowEqual),
   )
 }
