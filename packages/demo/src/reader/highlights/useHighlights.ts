e1e658b991970056f1cf36dbca088ca9099c0375
Maxime Bret <bret.maxime@gmail.com>
2024-08-20 22:11:00 +0200

    fix: fixed build

--- packages/demo/src/reader/highlights/useHighlights.ts
+++ packages/demo/src/reader/highlights/useHighlights.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
-import { tap } from "rxjs/operators"
-import { currentHighlight, isMenuOpenState } from "./states"
-import { ReaderInstance } from "./useCreateReader"
+// import { tap } from "rxjs/operators"
+// import { currentHighlight, isMenuOpenState } from "../states"
+import { ReaderInstance } from "../useCreateReader"
 
 export const useHighlights = (reader: ReaderInstance | undefined) => {
 
@@ -32,23 +32,23 @@ export const useHighlights = (reader: ReaderInstance | undefined) => {
   // }, [reader, setMenuOpenState])
 
   useEffect(() => {
-    const subscription = reader?.highlights.$.pipe(
-      tap((event) => {
-        if (event.type === `onHighlightClick`) {
-          currentHighlight.setValue(event.data)
-          isMenuOpenState.setValue(false)
-        }
+    // const subscription = reader?.highlights.$.pipe(
+    //   tap((event) => {
+    //     if (event.type === `onHighlightClick`) {
+    //       currentHighlight.setValue(event.data)
+    //       isMenuOpenState.setValue(false)
+    //     }
 
-        if (event.type === `onUpdate`) {
-          const toStore = event.data.map(({ anchorCfi, focusCfi, id }) => ({ anchorCfi, focusCfi, id }))
-          localStorage.setItem(`highlights`, JSON.stringify(toStore))
-        }
-      })
-    ).subscribe()
+    //     if (event.type === `onUpdate`) {
+    //       const toStore = event.data.map(({ anchorCfi, focusCfi, id }) => ({ anchorCfi, focusCfi, id }))
+    //       localStorage.setItem(`highlights`, JSON.stringify(toStore))
+    //     }
+    //   })
+    // ).subscribe()
 
-    return () => {
-      subscription?.unsubscribe()
-    }
+    // return () => {
+    //   subscription?.unsubscribe()
+    // }
   }, [reader])
 
   // create bookmarks enhancer and initialize with local storage bookmarks
