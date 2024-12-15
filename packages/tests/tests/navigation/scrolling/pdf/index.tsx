219ad1f25fc431d40783978c6d3bbdf65e42490f
mbret <bret.maxime@gmail.com>
2024-12-15 14:55:35 +0100

    feat: added tests for pdf navigation

---
+++ packages/tests/tests/navigation/scrolling/pdf/index.tsx
@@ -0,0 +1,45 @@
+import { createReader } from "@prose-reader/core"
+import { generateManifestFromArchive } from "@prose-reader/streamer"
+import { createArchiveFromPdf, pdfEnhancer } from "@prose-reader/enhancer-pdf"
+import * as pdfjsLib from "pdfjs-dist"
+import pdfWorkerUrl from "pdfjs-dist/build/pdf.worker.min.mjs?url"
+import { of } from "rxjs"
+import pdfjsViewerInlineCss from "pdfjs-dist/web/pdf_viewer.css?inline"
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(pdfWorkerUrl, import.meta.url).toString()
+
+async function run() {
+  const response = await fetch("http://localhost:3333/epubs/sample-3.pdf")
+  const pdfBlob = await response.blob()
+  const archive = await createArchiveFromPdf(pdfBlob)
+  const manifest = await generateManifestFromArchive(archive)
+
+  const createReaderWithEnhancers = pdfEnhancer(createReader)
+
+  const reader = createReaderWithEnhancers({
+    pageTurnAnimation: "none",
+    pageTurnMode: "scrollable",
+    layoutLayerTransition: false,
+    pdf: {
+      pdfjsViewerInlineCss,
+      getArchiveForItem: () => {
+        return of(archive)
+      },
+    },
+  })
+
+  reader.load({
+    containerElement: document.getElementById(`app`)!,
+    manifest,
+    cfi: localStorage.getItem(`cfi`) || undefined,
+  })
+
+  reader.pagination.state$.subscribe((state) => {
+    localStorage.setItem("cfi", state.beginCfi ?? ``)
+  })
+
+  // @ts-expect-error export for debug
+  window.reader = reader
+}
+
+run()
