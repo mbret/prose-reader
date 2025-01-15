811399c96be08b5894636131698ecab369d21f11
mbret <bret.maxime@gmail.com>
2025-01-15 22:47:52 +0100

    feat: migrated home

--- packages/demo/src/home/HomeScreen.tsx
+++ packages/demo/src/home/HomeScreen.tsx
@@ -1,14 +1,8 @@
-import { Link } from "react-router"
-import {
-  Button as ChakraButton,
-  Text,
-  Box,
-  Heading,
-  Link as ChakraLink,
-  Stack,
-} from "@chakra-ui/react"
-import { OrDivider } from "../common/OrDivider"
-import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { Link } from 'react-router';
+import { Text, Box, Heading, Link as ChakraLink, Stack } from '@chakra-ui/react';
+import { OrDivider } from '../common/OrDivider';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+import { Button } from '../components/ui/button';
 
 export const HomeScreen = () => {
   return (
@@ -21,64 +15,47 @@ export const HomeScreen = () => {
           alignItems: `center`,
         }}
       >
-        <Box position="relative">
-          <Heading
-            as="h1"
-            fontFamily="'Dancing Script', sans-serif"
-            fontSize="5rem"
-            mb={8}
-          >
+        <Box position="relative" mb={8}>
+          <Heading as="h1" fontFamily="'Dancing Script', sans-serif" size="6xl">
             prose
           </Heading>
-          <Text
-            as="span"
-            position="absolute"
-            top="10%"
-            right="-20%"
-            opacity={0.5}
-          >
+          <Text as="span" position="absolute" top="10%" right="-30%" opacity={0.5}>
             demo
           </Text>
         </Box>
         <Box textAlign="justify" maxWidth={[`auto`]} marginX={[4]} mb={4}>
           <Text as="p">
-            This demo is a reader made with React and use <b>prose</b> to render
-            its content.
+            This demo is a reader made with React and use <b>prose</b> to render its content.
           </Text>
           <Text as="p" mt={4}>
-            It provides a simple and generic user interface to showcase what{" "}
-            <b>prose</b> can do and how it integrates with a reader app.
+            It provides a simple and generic user interface to showcase what <b>prose</b> can do and
+            how it integrates with a reader app.
           </Text>
         </Box>
         <Box mt={4} mb={2} width="100%" paddingX={4}>
-          <Link to={`/books`} style={{ marginBottom: 20 }}>
-            <ChakraButton size="lg" width="100%">
+          <Button size="lg" width="100%" asChild>
+            <Link to={`/books`} style={{ marginBottom: 20 }}>
               Take me in!
-            </ChakraButton>
-          </Link>
+            </Link>
+          </Button>
         </Box>
         <OrDivider />
         <Box mt={2} width="100%" paddingX={4}>
-          <ChakraButton
-            size="lg"
-            width="100%"
-            as="a"
-            href="https://prose-reader.com/"
-            target="_blank"
-            rightIcon={<ExternalLinkIcon />}
-          >
-            landing page
-          </ChakraButton>
+          <Button size="lg" width="100%" asChild>
+            <ChakraLink href="https://prose-reader.com" target="_blank">
+              landing page <FaExternalLinkAlt />
+            </ChakraLink>
+          </Button>
         </Box>
       </Stack>
       <Box as="footer" paddingBottom={8} textAlign="center">
         <Text>
-          © Copyright{" "}
+          © Copyright{' '}
           <b>
             <ChakraLink
               href="https://www.linkedin.com/in/maxime-bret/"
               rel="nofollow noopener noreferrer"
-              isExternal
+              target="_blank"
             >
               Maxime Bret
             </ChakraLink>
@@ -86,5 +63,5 @@ export const HomeScreen = () => {
         </Text>
       </Box>
     </Stack>
-  )
-}
+  );
+};
