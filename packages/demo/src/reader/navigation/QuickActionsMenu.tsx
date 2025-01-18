c70c01b84d3fa5051dba85be0ab66cc977b9147e
mbret <bret.maxime@gmail.com>
2025-01-18 19:03:58 +0100

    feat: migrated quick action menu

--- packages/demo/src/reader/navigation/QuickActionsMenu.tsx
+++ packages/demo/src/reader/navigation/QuickActionsMenu.tsx
@@ -1,5 +1,5 @@
-import { EditIcon } from "@chakra-ui/icons"
-import { Fade, IconButton, Stack } from "@chakra-ui/react"
+import { FaEdit } from "react-icons/fa"
+import { IconButton, Stack } from "@chakra-ui/react"
 import { useReader } from "../useReader"
 import { filter, first, map, merge, of, switchMap } from "rxjs"
 import { useObserve } from "reactjrx"
@@ -26,30 +26,30 @@ export const QuickActionsMenu = () => {
   )
 
   return (
-    <Fade in={!!isOpen}>
-      <Stack
-        style={{
-          border: "1px solid red",
-          borderColor: "teal",
-          padding: 5,
-          borderRadius: 10,
-          position: "absolute",
-          bottom: "5%",
-          right: "5%",
+    <Stack
+      opacity={isOpen ? 1 : 0}
+      pointerEvents={isOpen ? "auto" : "none"}
+      transition="opacity"
+      style={{
+        border: "1px solid red",
+        borderColor: "teal",
+        padding: 5,
+        borderRadius: 10,
+        position: "absolute",
+        bottom: "5%",
+        right: "5%",
+      }}
+    >
+      <IconButton
+        onClick={() => {
+          isQuickMenuOpenSignal.setValue(false)
+          selectedHighlightSignal.setValue({
+            selection: reader?.selection.getSelection(),
+          })
         }}
       >
-        <IconButton
-          colorScheme="teal"
-          aria-label="Search database"
-          icon={<EditIcon />}
-          onClick={() => {
-            isQuickMenuOpenSignal.setValue(false)
-            selectedHighlightSignal.setValue({
-              selection: reader?.selection.getSelection(),
-            })
-          }}
-        />
-      </Stack>
-    </Fade>
+        <FaEdit />
+      </IconButton>
+    </Stack>
   )
 }
