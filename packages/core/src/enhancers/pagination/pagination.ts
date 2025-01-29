3ae8e7e0b9772442c41b0e2e70cef1bedffd076c
mbret <bret.maxime@gmail.com>
2025-01-29 22:40:01 +0100

    fix: fixed progressiont racking

--- packages/core/src/enhancers/pagination/pagination.ts
+++ packages/core/src/enhancers/pagination/pagination.ts
@@ -20,11 +20,13 @@ import type {
 import { isShallowEqual } from "../../utils/objects"
 import { trackTotalPages } from "./spine"
 import type { PaginationInfo } from "../../pagination/Pagination"
+import { Reader } from "../../reader"
 
 export const mapPaginationInfoToExtendedInfo = (
   reader: ReaderWithProgression,
   paginationInfo: PaginationInfo,
   chaptersInfo: ObservedValueOf<ReturnType<typeof trackChapterInfo>>,
+  percentageEstimateOfBook: number,
 ): Observable<
   Omit<
     ExtraPaginationInfo,
@@ -44,47 +46,62 @@ export const mapPaginationInfoToExtendedInfo = (
       ? reader.spineItemsManager.get(paginationInfo.endSpineItemIndex)
       : undefined
 
-  const endItemProgression$ = endItem
-    ? reader.progression.getPercentageEstimate(
-        context,
-        paginationInfo.endSpineItemIndex ?? 0,
-        paginationInfo.endNumberOfPagesInSpineItem,
-        paginationInfo.endPageIndexInSpineItem || 0,
-        reader.navigation.getNavigation().position,
-        endItem,
-      )
-    : of(0)
+  return of({
+    ...paginationInfo,
+    beginChapterInfo: beginItem ? chaptersInfo[beginItem.item.id] : undefined,
+    // chapterIndex: number;
+    // pages: number;
+    // pageIndexInBook: number;
+    // pageIndexInChapter: number;
+    // pagesOfChapter: number;
+    // pagePercentageInChapter: number;
+    // offsetPercentageInChapter: number;
+    // domIndex: number;
+    // charOffset: number;
+    // serializeString?: string;
+    beginSpineItemReadingDirection: beginItem?.readingDirection,
+    endChapterInfo: endItem ? chaptersInfo[endItem.item.id] : undefined,
+    endSpineItemReadingDirection: endItem?.readingDirection,
+    // spineItemReadingDirection: focusedSpineItem?.getReadingDirection(),
+    /**
+     * This percentage is based of the weight (kb) of every items and the number of pages.
+     * It is not accurate but gives a general good idea of the overall progress.
+     * It is recommended to use this progress only for reflow books. For pre-paginated books
+     * the number of pages and current index can be used instead since 1 page = 1 chapter.
+     */
+    percentageEstimateOfBook,
+    isUsingSpread: context.state.isUsingSpreadMode ?? false,
+    // hasNextChapter: (reader.spine.spineItemIndex || 0) < (manifest.readingOrder.length - 1),
+    // hasPreviousChapter: (reader.spine.spineItemIndex || 0) < (manifest.readingOrder.length - 1),
+    // numberOfSpineItems: context.manifest?.readingOrder.length,
+  })
+}
 
-  return endItemProgression$.pipe(
-    map((percentageEstimateOfBook) => ({
-      ...paginationInfo,
-      beginChapterInfo: beginItem ? chaptersInfo[beginItem.item.id] : undefined,
-      // chapterIndex: number;
-      // pages: number;
-      // pageIndexInBook: number;
-      // pageIndexInChapter: number;
-      // pagesOfChapter: number;
-      // pagePercentageInChapter: number;
-      // offsetPercentageInChapter: number;
-      // domIndex: number;
-      // charOffset: number;
-      // serializeString?: string;
-      beginSpineItemReadingDirection: beginItem?.readingDirection,
-      endChapterInfo: endItem ? chaptersInfo[endItem.item.id] : undefined,
-      endSpineItemReadingDirection: endItem?.readingDirection,
-      // spineItemReadingDirection: focusedSpineItem?.getReadingDirection(),
-      /**
-       * This percentage is based of the weight (kb) of every items and the number of pages.
-       * It is not accurate but gives a general good idea of the overall progress.
-       * It is recommended to use this progress only for reflow books. For pre-paginated books
-       * the number of pages and current index can be used instead since 1 page = 1 chapter.
-       */
-      percentageEstimateOfBook,
-      isUsingSpread: context.state.isUsingSpreadMode ?? false,
-      // hasNextChapter: (reader.spine.spineItemIndex || 0) < (manifest.readingOrder.length - 1),
-      // hasPreviousChapter: (reader.spine.spineItemIndex || 0) < (manifest.readingOrder.length - 1),
-      // numberOfSpineItems: context.manifest?.readingOrder.length,
-    })),
+const observeProgression = (reader: ReaderWithProgression) => {
+  return combineLatest([
+    reader.pagination.state$,
+    // Usually pagination change if layout changes (number of pages) however it is especially
+    // useful for like webtoon where you still have one page but only the layout will give you the final size
+    // although the pagination will stay stable from the begining
+    reader.layout$,
+  ]).pipe(
+    switchMap(([paginationInfo]) => {
+      const endItem =
+        paginationInfo.endSpineItemIndex !== undefined
+          ? reader.spineItemsManager.get(paginationInfo.endSpineItemIndex)
+          : undefined
+
+      return endItem
+        ? reader.progression.getPercentageEstimate(
+            reader.context,
+            paginationInfo.endSpineItemIndex ?? 0,
+            paginationInfo.endNumberOfPagesInSpineItem,
+            paginationInfo.endPageIndexInSpineItem || 0,
+            reader.navigation.getNavigation().position,
+            endItem,
+          )
+        : of(0)
+    }),
   )
 }
 
@@ -108,11 +125,18 @@ export const trackPaginationInfo = (reader: ReaderWithProgression) => {
     percentageEstimateOfBook: 0,
   })
 
-  const extandedBasePagination$ = reader.pagination.state$.pipe(
-    combineLatestWith(chaptersInfo$),
-
-    switchMap(([info, chaptersInfo]) =>
-      mapPaginationInfoToExtendedInfo(reader, info, chaptersInfo).pipe(
+  const extandedBasePagination$ = combineLatest([
+    reader.pagination.state$,
+    chaptersInfo$,
+    observeProgression(reader),
+  ]).pipe(
+    switchMap(([info, chaptersInfo, percentageEstimateOfBook]) =>
+      mapPaginationInfoToExtendedInfo(
+        reader,
+        info,
+        chaptersInfo,
+        percentageEstimateOfBook,
+      ).pipe(
         map((extendedInfo) => ({
           ...info,
           ...extendedInfo,
