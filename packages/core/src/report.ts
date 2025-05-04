4634734dc798347e074b125b0265d74e283dc880
mbret <bret.maxime@gmail.com>
2025-05-05 00:08:52 +0200

    fix: lint

--- packages/core/src/report.ts
+++ packages/core/src/report.ts
@@ -2,6 +2,4 @@ const ROOT_NAMESPACE = `@prose-reader/core`
 
 import { Report as SharedReport } from "@prose-reader/shared"
 
-export const Report = SharedReport.namespace(
-  ROOT_NAMESPACE,
-)
+export const Report = SharedReport.namespace(ROOT_NAMESPACE)
