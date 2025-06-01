5d1e09932b9c491672ecb474b1b11b6da9d31aca
mbret <bret.maxime@gmail.com>
2025-06-02 00:37:34 +0200

    feat: cleanup

--- prose-reader-react-native-demo/app/_layout.tsx
+++ prose-reader-react-native-demo/app/_layout.tsx
@@ -19,8 +19,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" options={{ headerShown: true }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
