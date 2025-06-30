07c24138f298160dd898618b7792b4627920a483
mbret <bret.maxime@gmail.com>
2025-06-30 12:28:00 +0200

    fix: oupsie

--- packages/enhancer-gestures/src/utils.ts
+++ packages/enhancer-gestures/src/utils.ts
@@ -21,3 +21,24 @@ export const getPositionRelativeToContainer = (
     y: y - top,
   }
 }
+
+export const istMatchingSelectors = (
+  selectors: string[],
+  event: GestureEvent["event"],
+): boolean => {
+  const target = event.event.target
+
+  if (!isHtmlElement(target)) return false
+
+  const match = selectors.find((selector) => {
+    // Check if the target matches the selector directly
+    if (target.matches(selector)) return true
+
+    // Check if the target is within an element matching the selector
+    if (target.closest(selector)) return true
+
+    return false
+  })
+
+  return !!match
+}
