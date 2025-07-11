76aae87936542681f07b36c1d833bae7fddcbb6c
mbret <bret.maxime@gmail.com>
2025-07-11 23:53:34 +0200

    feat: added refit package

---
+++ packages/enhancer-refit/src/index.ts
@@ -0,0 +1,110 @@
+import {
+  HTML_ATTRIBUTE_DATA_READER_ID,
+  HTML_ATTRIBUTE_VIEWPORT_CLASSNAME,
+  ReactiveEntity,
+  type Reader,
+} from "@prose-reader/core"
+import {
+  combineLatest,
+  distinctUntilChanged,
+  map,
+  type Observable,
+  tap,
+} from "rxjs"
+
+export type RefitEnhancerAPI = {
+  readonly __PROSE_READER_ENHANCER_REFIT: boolean
+  refit: {
+    update: (settings: Partial<RefitEnhancerOptions>) => void
+    settings$: Observable<RefitEnhancerOptions>
+  }
+}
+
+export type RefitEnhancerOptions = {
+  viewportFit?: "desktop" | "tablet" | "fit" | "custom"
+  customWidth?: number
+}
+
+class Settings extends ReactiveEntity<RefitEnhancerOptions> {
+  update(settings: RefitEnhancerOptions) {
+    super.mergeCompare(settings)
+  }
+}
+
+export const refitEnhancer =
+  <InheritOptions, InheritOutput extends Reader>(
+    next: (options: InheritOptions) => InheritOutput,
+  ) =>
+  (
+    options: InheritOptions & {
+      refit?: Partial<RefitEnhancerOptions>
+    },
+  ): InheritOutput & RefitEnhancerAPI => {
+    const { refit = {}, ...rest } = options
+    const reader = next(rest as InheritOptions)
+    const settings = new Settings(refit)
+    const stylesheetId = `prose-reader-refit-${reader.id}`
+    const maxWidthCssVariable = `--${stylesheetId}-max-width`
+
+    const style = document.createElement("style")
+    style.id = stylesheetId
+    style.textContent = `
+      [${HTML_ATTRIBUTE_DATA_READER_ID}] .${HTML_ATTRIBUTE_VIEWPORT_CLASSNAME} {
+        border: 1px solid red;
+        margin: auto;
+        width: 100%;
+        max-width: var(${maxWidthCssVariable});
+      }
+    `
+
+    document.head.appendChild(style)
+
+    const viewportFitSubscription = combineLatest([
+      settings.watch(["viewportFit", "customWidth"]),
+      reader.settings.watch(["computedPageTurnMode"]),
+    ])
+      .pipe(
+        map(([{ viewportFit, customWidth }, { computedPageTurnMode }]) => {
+          if (computedPageTurnMode === "scrollable") {
+            if (viewportFit === "desktop") {
+              return "800px"
+            }
+
+            if (viewportFit === "tablet") {
+              return "600px"
+            }
+
+            if (viewportFit === "custom") {
+              return `${customWidth ?? 100}%`
+            }
+          }
+
+          return "100%"
+        }),
+        distinctUntilChanged(),
+        tap((maxWidth) => {
+          document.documentElement.style.setProperty(
+            maxWidthCssVariable,
+            maxWidth,
+          )
+          reader.layout()
+        }),
+      )
+      .subscribe()
+
+    const destroy = () => {
+      viewportFitSubscription.unsubscribe()
+      document.head.removeChild(style)
+      reader.destroy()
+    }
+
+    return {
+      ...reader,
+      __PROSE_READER_ENHANCER_REFIT: true,
+      destroy,
+      refit: {
+        update: settings.update.bind(settings),
+        settings$: settings,
+      },
+    }
+  }
---
+++ packages/enhancer-refit/vite.config.ts
@@ -0,0 +1,34 @@
+import { resolve } from "node:path"
+import externals from "rollup-plugin-node-externals"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+import { name } from "./package.json"
+
+const libName = name.replace(`@`, ``).replace(`/`, `-`)
+
+export default defineConfig(({ mode }) => ({
+  build: {
+    minify: false,
+    target: "esnext",
+    lib: {
+      entry: resolve(__dirname, `src/index.ts`),
+      name: libName,
+      fileName: `index`,
+    },
+    emptyOutDir: mode !== `development`,
+    sourcemap: true,
+  },
+  plugins: [
+    {
+      enforce: `pre`,
+      ...externals({
+        peerDeps: true,
+        deps: true,
+        devDeps: true,
+      }),
+    },
+    dts({
+      entryRoot: "src",
+    }),
+  ],
+}))
