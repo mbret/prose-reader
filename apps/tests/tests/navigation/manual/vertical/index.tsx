5794767ed04c6eb69c9824f07037ca1ac7ad8823
mbret <bret.maxime@gmail.com>
2025-07-12 16:57:05 +0200

    feat: added more tests

---
+++ apps/tests/tests/navigation/manual/vertical/index.tsx
@@ -0,0 +1,51 @@
+import { createReader } from "@prose-reader/core"
+import { createArchiveFromJszip, Streamer } from "@prose-reader/streamer"
+import { loadAsync } from "jszip"
+import { from } from "rxjs"
+
+async function createStreamer() {
+  const streamer = new Streamer({
+    getArchive: async () => {
+      const epubResponse = await fetch("http://localhost:3333/epubs/sample.cbz")
+      const epubBlob = await epubResponse.blob()
+      const epubJszip = await loadAsync(epubBlob)
+      const archive = await createArchiveFromJszip(epubJszip)
+
+      return archive
+    },
+  })
+
+  return streamer
+}
+
+async function run() {
+  const streamer = await createStreamer()
+  const manifestResponse = await streamer.fetchManifest({
+    key: `_`,
+  })
+  const manifest = await manifestResponse.json()
+
+  const reader = createReader({
+    pageTurnAnimation: "none",
+    pageTurnDirection: "vertical",
+    layoutLayerTransition: false,
+    getResource: (item) => {
+      return from(streamer.fetchResource({ key: `_`, resourcePath: item.href }))
+    },
+  })
+
+  const query = new URLSearchParams(window.location.search)
+  const cfi = query.get("cfi") || undefined
+
+  reader.load({
+    // biome-ignore lint/style/noNonNullAssertion: TODO
+    containerElement: document.getElementById(`app`)!,
+    manifest,
+    cfi,
+  })
+
+  // @ts-expect-error export for debug
+  window.reader = reader
+}
+
+run()
---
+++ apps/tests/tests/navigation/manual/vertical/turn.spec.ts
@@ -0,0 +1,36 @@
+import { test } from "@playwright/test"
+import type { Reader } from "@prose-reader/core"
+import {
+  expectSpineItemsInViewport,
+  waitForSpineItemReady,
+} from "../../../utils"
+
+test.describe(`Given vertical controlled book`, () => {
+  test.describe("When the user turns bottom", () => {
+    test("should navigate to the right page", async ({ page }) => {
+      await page.setViewportSize({
+        width: 300,
+        height: 600,
+      })
+
+      await page.goto(
+        `http://localhost:3333/tests/navigation/manual/vertical/index.html`,
+      )
+
+      // Wait for both spine items to be ready
+      await waitForSpineItemReady(page, [0])
+
+      await page.evaluate(() => {
+        // @ts-ignore
+        const reader = window.reader as Reader
+
+        reader.navigation.turnRightOrBottom()
+      }, [])
+
+      await expectSpineItemsInViewport({
+        page,
+        indexes: [1],
+      })
+    })
+  })
+})
