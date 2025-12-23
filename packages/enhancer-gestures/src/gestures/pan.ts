044a1ab447ce0f795ad2098f4c84be483dcf4376
mbret <bret.maxime@gmail.com>
2025-12-23 17:17:27 +0100

    feat: added pan zoom

--- packages/enhancer-gestures/src/gestures/pan.ts
+++ packages/enhancer-gestures/src/gestures/pan.ts
@@ -30,61 +30,83 @@ export const registerPan = ({
 
       const pan$ = panStart$.pipe(
         switchMap((panStartEvent) => {
-          const startZoomPosition = reader.zoom.state.currentPosition
+          /**
+           * We use the last cumulative delta to derive the new event atomic delta.
+           * This is because panning the zoom does not necessarily means the zoom position
+           * will always changes. If the user keep dragging while the zoom is blocked, we want
+           * it to move the other direction when he start dragging the other way.
+           * We cannot use the `reader.zoom.state.currentPosition` as previous position
+           * and the event.deltaX to compute the new zoom position.
+           */
+          let lastDelta = { x: 0, y: 0 }
 
           const moveAndEnd$ = merge(panMove$, panEnd$).pipe(
             map((event) => {
               const isZooming = reader.zoom.state.isZooming
               const isZoomingIn = reader.zoom.state.currentScale > 1
 
+              /**
+               * When user is zooming in, we don't navigate anymore.
+               * The gestures is gonna be handled by the pinch and viewport.
+               */
               if (isZooming && isZoomingIn) {
-                const x = startZoomPosition.x + Math.floor(event.deltaX)
-                const y = startZoomPosition.y + Math.floor(event.deltaY)
+                const deltaX = event.deltaX - lastDelta.x
+                const deltaY = event.deltaY - lastDelta.y
 
-                reader.zoom.move({
-                  x,
-                  y,
-                })
+                lastDelta = {
+                  x: event.deltaX,
+                  y: event.deltaY,
+                }
 
-                return { event, handled: true }
+                reader.zoom.move(
+                  {
+                    x: reader.zoom.state.currentPosition.x + deltaX,
+                    y: reader.zoom.state.currentPosition.y + deltaY,
+                  },
+                  {
+                    constrain: "within-viewport",
+                  },
+                )
+
+                return event
               }
 
               if (event.type === `panMove`) {
-                reader.navigation.moveTo({
+                if (!reader.navigation.panNavigator.value.isStarted) {
+                  reader.navigation.panNavigator.start({
+                    x: event.deltaX,
+                    y: event.deltaY,
+                  })
+
+                  return event
+                }
+
+                reader.navigation.panNavigator.panMoveTo({
                   x: event.deltaX,
                   y: event.deltaY,
                 })
 
-                return { event, handled: true }
+                return event
               }
 
-              if (event.type === `panEnd`) {
-                reader.navigation.moveTo(
-                  { x: event.deltaX, y: event.deltaY },
-                  { final: true },
-                )
+              if (
+                event.type === `panEnd` &&
+                reader.navigation.panNavigator.value.isStarted
+              ) {
+                reader.navigation.panNavigator.stop({
+                  x: event.deltaX,
+                  y: event.deltaY,
+                })
 
-                return { event, handled: true }
+                return event
               }
 
-              return { event, handled: false }
+              return event
             }),
           )
 
-          const isZoomingIn = reader.zoom.state.currentScale > 1
-
-          if (!isZoomingIn) {
-            reader?.navigation.moveTo({ x: 0, y: 0 }, { start: true })
-
-            return merge(
-              of({ event: panStartEvent, handled: true }),
-              moveAndEnd$,
-            )
-          }
-
-          return merge(
-            of({ event: panStartEvent, handled: false }),
-            moveAndEnd$,
+          return merge(of(panStartEvent), moveAndEnd$).pipe(
+            map((event) => ({ type: "pan" as const, gestureEvent: event })),
           )
         }),
       )
