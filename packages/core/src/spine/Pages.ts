8c1951aa695309d8a9b27c21cb525516960546d1
mbret <bret.maxime@gmail.com>
2026-03-04 10:15:16 +0100

    feat: improve performances on pages layout

--- packages/core/src/spine/Pages.ts
+++ packages/core/src/spine/Pages.ts
@@ -1,8 +1,6 @@
 import {
-  combineLatest,
   map,
-  type Observable,
-  of,
+  Observable,
   share,
   switchMap,
   takeUntil,
@@ -14,7 +12,6 @@ import type { SpineItem } from "../spineItem/SpineItem"
 import { SpineItemPageLayout } from "../spineItem/types"
 import { getFirstVisibleNodeForPositionRelativeTo } from "../utils/dom"
 import { ReactiveEntity } from "../utils/ReactiveEntity"
-import { idle } from "../utils/rxjs"
 import type { Viewport } from "../viewport/Viewport"
 import type { SpineLocator } from "./locator/SpineLocator"
 import { Report } from "./report"
@@ -53,6 +50,108 @@ export type PagesState = {
   pages: PageEntry[]
 }
 
+type UnresolvedPageEntry = Omit<PageEntry, "firstVisibleNode"> & {
+  spineItem: SpineItem
+}
+
+// Chunk heavy first-visible-node lookups to keep frame time predictable.
+const FIRST_VISIBLE_NODE_PAGES_PER_FRAME = 10
+
+const resolveFirstVisibleNodeForPage = (
+  page: UnresolvedPageEntry,
+): PageEntry => {
+  const { spineItem: _unused, ...rest } = page
+
+  if (!page.spineItem.value.isLoaded) {
+    return {
+      ...rest,
+      firstVisibleNode: undefined,
+    }
+  }
+
+  const frame = page.spineItem.renderer?.getDocumentFrame()
+  const frameDocument = frame?.contentWindow?.document
+
+  if (!frameDocument || frameDocument.body === null) {
+    return {
+      ...rest,
+      firstVisibleNode: undefined,
+    }
+  }
+
+  return {
+    ...rest,
+    firstVisibleNode: getFirstVisibleNodeForPositionRelativeTo(
+      frameDocument,
+      page.layout,
+    ),
+  }
+}
+
+const resolvePagesFirstVisibleNodeInFrames$ = (
+  pages: UnresolvedPageEntry[],
+) => {
+  return new Observable<PageEntry[]>((observer) => {
+    if (pages.length === 0) {
+      observer.next([])
+      observer.complete()
+      return
+    }
+
+    let frameRequestId: number | undefined
+    let nextPageIndex = 0
+
+    const resolvedPages = new Array<PageEntry>(pages.length)
+
+    const clearScheduled = () => {
+      if (frameRequestId !== undefined) {
+        cancelAnimationFrame(frameRequestId)
+        frameRequestId = undefined
+      }
+    }
+
+    const processChunk = () => {
+      frameRequestId = undefined
+
+      let processedCount = 0
+
+      while (
+        nextPageIndex < pages.length &&
+        processedCount < FIRST_VISIBLE_NODE_PAGES_PER_FRAME
+      ) {
+        const page = pages[nextPageIndex]
+
+        if (!page) {
+          nextPageIndex = pages.length
+          break
+        }
+
+        resolvedPages[nextPageIndex] = resolveFirstVisibleNodeForPage(page)
+        nextPageIndex += 1
+        processedCount += 1
+      }
+
+      if (nextPageIndex >= pages.length) {
+        observer.next(resolvedPages)
+        observer.complete()
+        return
+      }
+
+      scheduleNextChunk()
+    }
+
+    const scheduleNextChunk = () => {
+      frameRequestId = requestAnimationFrame(processChunk)
+    }
+
+    scheduleNextChunk()
+
+    return () => {
+      clearScheduled()
+    }
+  })
+}
+
 /**
  * Hold relevant information about spine pages.
  */
@@ -72,17 +171,13 @@ export class Pages extends ReactiveEntity<PagesState> {
     this.layout$ = spineLayout.layout$.pipe(
       withLatestFrom(viewport),
       switchMap(([, { pageSize }]) => {
-        const pages = spineItemsManager.items.reduce(
-          (
-            acc: (Omit<PageEntry, "firstVisibleNode"> & {
-              spineItem: SpineItem
-            })[],
-            spineItem,
-            itemIndex,
-          ) => {
-            const pages = new Array(spineItem.numberOfPages).fill(undefined)
-
-            const pagesAbsolutePositions = pages.map((_, pageIndex) => {
+        const pages = spineItemsManager.items.reduce<UnresolvedPageEntry[]>(
+          (acc, spineItem, itemIndex) => {
+            for (
+              let pageIndex = 0;
+              pageIndex < spineItem.numberOfPages;
+              pageIndex += 1
+            ) {
               // @todo handle vertical jp
               // top seems ok but left is not, it should probably not be 0 or something
               const pageSpineItemPosition =
@@ -97,7 +192,9 @@ export class Pages extends ReactiveEntity<PagesState> {
                   spineItemPosition: pageSpineItemPosition,
                 })
 
-              return {
+              const absolutePageIndex = acc.length
+
+              acc.push({
                 absoluteLayout: spinePositionToSpineItemSpineLayout({
                   pageSize,
                   position: pageSpinePosition,
@@ -113,67 +210,18 @@ export class Pages extends ReactiveEntity<PagesState> {
                   y: pageSpineItemPosition.y,
                 }),
                 itemIndex,
-                absolutePageIndex: acc.length + pageIndex,
+                absolutePageIndex,
                 spineItem,
                 pageIndex,
-              }
-            })
+              })
+            }
 
-            return [...acc, ...pagesAbsolutePositions]
+            return acc
           },
           [],
         )
 
-        const pages$ = combineLatest(
-          pages.map((page) => {
-            const { spineItem: _unused, ...rest } = page
-
-            /**
-             * Note that this part adds a significant overhead
-             * - lot of rxjs subscribers
-             * - overhead of the idle browser task chain
-             * - overhead with node finding
-             * This is directly tied to the number of items the user loads in parallel
-             * and the size of the book.
-             * However there are realistically no reason a user would want to load
-             * a massive book entirely in parallel.
-             */
-            if (page.spineItem.value.isLoaded) {
-              const frame = page.spineItem.renderer?.getDocumentFrame()
-
-              if (
-                frame &&
-                frame?.contentWindow?.document &&
-                frame.contentWindow.document.body !== null
-              ) {
-                const _document = frame.contentWindow.document
-
-                // might need to buffer them to run them all within a single idle task.
-                // not sure stacking lot of them like this is a good idea. additionally, maybe we can
-                // use something more efficient if its just to ensure 60fps.
-                return idle().pipe(
-                  map(() => {
-                    const firstVisibleNode =
-                      getFirstVisibleNodeForPositionRelativeTo(
-                        _document,
-                        page.layout,
-                      )
-
-                    return {
-                      ...rest,
-                      firstVisibleNode,
-                    }
-                  }),
-                )
-              }
-            }
-
-            // Fast path: No overhead for majority of pages
-            return of({ ...rest, firstVisibleNode: undefined })
-          }),
-        )
-
-        return pages$
+        return resolvePagesFirstVisibleNodeInFrames$(pages)
       }),
       map((pages) => {
         Report.info(`Pages layout`, pages)
